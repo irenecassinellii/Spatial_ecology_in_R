09975f58db6abcbebaf27fc5b7646659737f0684
irenecassinellii <146854176+irenecassinellii@users.noreply.github.com>
2023-10-05 17:59:11 +0200

    Update Lecture_2_populations.R

--- Lecture_2_populations.R
+++ Lecture_2_populations.R
@@ -2,4 +2,34 @@
 
 # a package is needed for point pattern analyses 
 install.packages ("spatstat") # to install packages external from R, "" are needed when typing something external from R
-library (spatstat) # to check if the package has been installed
+library (spatstat) # to check if the package has been installed and to use it
+
+# let's use bei data
+# data description:
+# HTTP://CRAN.R-project.org
+
+bei
+
+# to plot bei data
+plot (bei)
+
+# to change character dimensions
+plot (bei, cex=0.5)
+
+# to change symbol 
+plot (bei, cex = 0.4, pch = 19)
+
+# additional dataset
+bei.extra
+plot (bei.extra)
+
+# let's use only a part of the dataset: elevation
+plot (bei.extra$elev)
+
+# to assign bei.extra$elev to a new object
+elevation <- bei.extra$elev
+plot (elevation)
+
+# second method to select elements
+elevation2 <- bei.extra[[1]]
+plot (elevation2)
