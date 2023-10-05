deec28ca23c699d3c0b19edcd438e9a1b7d4ad9c
irenecassinellii <146854176+irenecassinellii@users.noreply.github.com>
2023-10-05 17:23:26 +0200

    Create Lecture_2_populations.R

---
+++ Lecture_2_populations.R
@@ -0,0 +1,5 @@
+# Code reated to population ecology
+
+# a package is needed for point pattern analyses 
+install.packages ("spatstat") # to install packages external from R, "" are needed when typing something external from R
+library (spatstat) # to check if the package has been installed
