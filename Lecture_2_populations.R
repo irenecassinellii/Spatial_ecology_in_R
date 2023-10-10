5ce183d4cb7c78601c9b55a8228f9e101664b4b7
irenecassinellii <146854176+irenecassinellii@users.noreply.github.com>
2023-10-10 17:43:56 +0200

    Update Lecture_2_populations.R

--- Lecture_2_populations.R
+++ Lecture_2_populations.R
@@ -33,3 +33,27 @@ plot (elevation)
 # second method to select elements
 elevation2 <- bei.extra[[1]]
 plot (elevation2)
+
+# passing from points to a continuous surface
+densitymap <- density(bei)
+densitymap
+plot (densitymap)
+
+# to put points on the image
+points (bei, cex=0.2)
+
+# to change the color palette 
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(100)
+plot (densitymap, col=cl)
+
+# to visualize only 4 colors
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(4)
+plot (densitymap, col=cl)
+
+# multiframe
+plot (bei.extra)
+elevation <- bei.extra[[1]]
+plot (elevation)
+par(mfrow=c(1,2))
+plot(densitymap)
+plot(elev)
