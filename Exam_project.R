267430d37cf39cb1afd1ceec9ef437c653fe7512
irenecassinellii <146854176+irenecassinellii@users.noreply.github.com>
2024-01-29 19:59:28 +0100

    Update Exam_project.R

--- Exam_project.R
+++ Exam_project.R
@@ -1,7 +1,7 @@
 # Exam project: Amazon deforestation
 
 # Analysis of the Amazon vegetation cover as a result of deforestation 
-# This project aims to study the decrease in vegetation cover in the Amazon rainforest caused by deforestation between 2000, 2006, and 2012
+# This project aims to study the decrease in vegetation cover in the Amazon rainforest caused by deforestation between 2000 and 2012
 
 # The images used for this project were downloaded from the NASA website (https://earthobservatory.nasa.gov/world-of-change/Deforestation)
 
@@ -17,13 +17,11 @@ setwd("C:/Users/irene/Desktop")
 
 # Use the brick() function of the raster package to display the images
 amazon2000 <- brick("Amazon2000.jpg")
-amazon2006 <- brick("Amazon2006.jpg")
 amazon2012 <- brick("Amazon2012.jpg")
 
-# Use the function par() to arrange the three graphs in a 3x1 grid (i.e., one below the other)
-par(mfrow=c(3,1))
+# Use the function par() to arrange the three graphs in a 2x1 grid (i.e., one below the other)
+par(mfrow=c(2,1))
 plotRGB(amazon2000, r=1, g=2, b=3)
-plotRGB(amazon2006, r=1, g=2, b=3)
 plotRGB(amazon2012, r=1, g=2, b=3)
 
 # To cut the images install the packages "jpeg" and "magick"
@@ -36,7 +34,6 @@ library(magick)
 
 # Upload jpg images
 img2000 <- image_read("Amazon2000.jpg")
-img2006 <- image_read("Amazon2006.jpg")
 img2012 <- image_read("Amazon2012.jpg")
 
 # Specify the co-ordinates of the crop rectangle (x1, y1, width, height)
@@ -47,12 +44,10 @@ height <- 1800
 
 # Crop the image with the function image_crop() and use the paste() function to define the new image geometry
 img2000_cropped <- image_crop(img2000, geometry = paste0(width, "x", height, "+", x1, "+", y1))
-img2006_cropped <- image_crop(img2006, geometry = paste0(width, "x", height, "+", x1, "+", y1))
 img2012_cropped <- image_crop(img2012, geometry = paste0(width, "x", height, "+", x1, "+", y1))
 
 # and save cropped images with the function image_write()
 image_write(img2000_cropped, path = "Amazon2000.jpg")
-image_write(img2006_cropped, path = "Amazon2006.jpg")
 image_write(img2012_cropped, path = "Amazon2012.jpg")
 
 # Use the dev.off() function to clean the console 
@@ -62,11 +57,14 @@ dev.off()
 setwd("C:/Users/irene/Desktop")
 
 amazon2000 <- brick("Amazon2000.jpg")
-amazon2006 <- brick("Amazon2006.jpg")
 amazon2012 <- brick("Amazon2012.jpg")
 
-par(mfrow=c(3,1))
+par(mfrow=c(2,1))
 plotRGB(amazon2000, r=1, g=2, b=3)
-plotRGB(amazon2006, r=1, g=2, b=3)
 plotRGB(amazon2012, r=1, g=2, b=3)
 
+# Multitemporal change detection
+amazondif = amazon2000[[1]] - amazon2012[[1]]
+
+cl <- colorRampPalette(c("brown", "grey", "orange")) (100)
+plot(amazondif, col=cl)
