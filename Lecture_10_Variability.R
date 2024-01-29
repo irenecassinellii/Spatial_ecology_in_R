5cf5330c207e23421ae4c4260e6336d9d59f059b
irenecassinellii <146854176+irenecassinellii@users.noreply.github.com>
2024-01-29 13:07:44 +0100

    Update Lecture_10_Variability.R

--- Lecture_10_Variability.R
+++ Lecture_10_Variability.R
@@ -38,3 +38,35 @@ plot(sd7, col=viridisc)
 # original image plus the 7x7 sd
 im.plotRGB(sent, r=2, g=1, b=3)
 plot(sd7, col=viridisc)
+
+im.list()
+sent <- im.import("sentinel.png")
+pairs(sent)
+
+dev.off()
+
+sentpc <- im.pca2 (sent)
+pc1 <- sentpc$PC1
+
+plot(pc1, col=viridisc)
+
+pc1sd3 <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
+plot(pc1sd3, col=viridisc)
+
+pc1sd7 <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
+plot(pc1sd7, col=viridisc)
+
+par(mfrow=c(2, 3)) # 2 rows, 3 columns
+im.plotRGB(sent, 2, 1, 3)
+plot(sd3, col=viridisc)
+plot(sd7, col=viridisc)
+plot(pc1, col=viridisc)
+plot(pc1sd3, col=viridisc)
+plot(pc1sd7, col=viridisc)
+
+sdstack <- c(sd3, sd7, pc1sd3, pc1sd7)
+plot(sdstack, col=viridisc)
+
+names(sdstack) <- c("sd3", "sd7", "pc1sd3", "pc1sd7")
+plot(sdstack, col=viridisc)
+# standard deviation represents the variability among the moving windows
