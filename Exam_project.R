54f90a3e92950253542ada8356b515e4d1cd62af
irenecassinellii <146854176+irenecassinellii@users.noreply.github.com>
2024-01-30 23:57:26 +0100

    Update Exam_project.R

--- Exam_project.R
+++ Exam_project.R
@@ -27,34 +27,49 @@ library(magick)
 setwd("C:/Users/irene/Desktop")
 
 # Use the brick() function of the raster package to display the images
-amazon2001 <- brick("Amazon2001.jpg")
-amazon2012 <- brick("Amazon2012.jpg")
+og_amazon2001 <- brick("Amazon2001.jpg")
+og_amazon2012 <- brick("Amazon2012.jpg")
 
-amazon2001 <- image_read("Amazon2001.jpg")
-amazon2001_cropped <- image_crop(img, geometry="200x200+100+100")
+# Crop the original images 
+og_amazon2001 <- image_read("Amazon2001.jpg")
+amazon2001_cropped <- image_crop(og_amazon2001, geometry="2500x2000+500+500")
 image_write(amazon2001_cropped, path="amazon2001_cropped.jpg")
 
+og_amazon2012 <- image_read("Amazon2012.jpg")
+amazon2012_cropped <- image_crop(og_amazon2012, geometry="2500x2000+500+500")
+image_write(amazon2012_cropped, path="amazon2012_cropped.jpg")
+
+amazon2001 <- brick("amazon2001_cropped.jpg")
+amazon2012 <- brick("amazon2012_cropped.jpg")
 
 # Use the function par() to arrange the two graphs in a 2x1 grid (i.e., one below the other)
 # and create a multi-frame with 2000 and 2012 Amazon images with plotRGB() function
 par(mfrow=c(2,1))
-plotRGB(amazon2000, r=1, g=2, b=3)
+plotRGB(amazon2001, r=1, g=2, b=3)
 plotRGB(amazon2012, r=1, g=2, b=3)
 
 ## Bands: 1 = Near-InfraRed (NIR), 2 = Red (R), 3 = Green (G)
 
-# Clean the graphic visualization with the dev.off() function
+# Save the two images with the jpeg() function 
+jpeg("amazon2001RGB.jpg")
+plotRGB(amazon2001, r=1, g=2, b=3)
+dev.off()
+
+jpeg("amazon2012RGB.jpg")
+plotRGB(amazon2012, r=1, g=2, b=3)
 dev.off()
 
 # Calculate the Amazon Difference Vegetation Index (DVI) of 2000
-dvi2000 = amazon2000[[1]] - amazon2000[[2]]
-plot(dvi2000)
+dvi2001 = amazon2001[[1]] - amazon2001[[2]]
+plot(dvi2001)
 
 ## DVI = NIR - R 
 
 # To effectively visualize raster data use the colourRampPalette() function to define new image colours
 cl <- colorRampPalette(c("darkblue", "white", "red", "black"))(100)
-plot (dvi2000, col=cl)
+plot (dvi2001, col=cl, zlim=c(-40, 80))
+
+## zlim() function is used to define the scale of the graph
 
 # Calculate the DVI of 2012 
 dvi2012 = amazon2012[[1]] - amazon2012[[2]]
@@ -69,68 +84,54 @@ plot (dvi2012, col=cl)
 
 # Visualize the two graphs in a 2x1 grid and compare them
 par(mfrow=c(1, 2))
-plot (dvi2000, col=cl)
-plot (dvi2012, col=cl)
+plot (dvi2001, col=cl, zlim=c(-40, 80))
+plot (dvi2012, col=cl, zlim=c(-40, 80))
 
 # Clean the current graphic visualization
 dev.off()
 
 # Calculate the Normalized Difference Vegetation Index (NDVI) of 2000
-ndvi2000 =  (amazon2000[[1]] - amazon2000[[2]]) / (amazon2000[[1]] + amazon2000[[2]])
-ndvi2000 = dvi2000 / (amazon2000[[1]] + amazon2000[[2]])
-plot (ndvi2000, col=cl)
+ndvi2001 =  (amazon2001[[1]] - amazon2001[[2]]) / (amazon2001[[1]] + amazon2001[[2]])
+ndvi2001 = dvi2001 / (amazon2001[[1]] + amazon2001[[2]])
+plot (ndvi2001, col=cl, zlim=c(-1, 1))
 
 # Calculate the NDVI of 2012
 ndvi2012 = (amazon2012[[1]] - amazon2012[[2]]) / (amazon2012[[1]] + amazon2012[[2]])
 ndvi2012 = dvi2012 / (amazon2012[[1]] + amazon2012[[2]])
-plot (ndvi2012, col=cl)
+plot (ndvi2012, col=cl, zlim=c(-1, 1))
 
 ## NDVI is a normalized index that provides standardized values that can be
 ## more easily interpreted and compared between different images or areas.
 
-# Now plot and compare the two graphs
-par(mfrow=c(1,2))
-plot(ndvi2000, col=cl)
-plot(ndvi2012, col=cl)
-
-dev.off()
-
-# Make it colorblind approved
+# Now plot and compare the two graphs and make it colorblind approved
 viridis <- colorRampPalette(viridis(7))(255)
-plot(ndvi2000, col=viridis)
-plot(ndvi2012, col=viridis)
-
-# and plot the two graphs together, creating a multi-frame
 par(mfrow=c(1,2))
-plot(ndvi2000, col=viridis)
-plot(ndvi2012, col=viridis)
+plot(ndvi2001, col=viridis(2), zlim=c(-1, 1))
+plot(ndvi2012, col=viridis(2), zlim=c(-1, 1))
 
 dev.off()
 
 # Calculate the multi-temporal change detection in vegetation cover between 2000 and 2012
-amazondif = amazon2000[[1]] - amazon2012[[1]]
+amazondif = amazon2001[[1]] - amazon2012[[1]]
 
 ## Multi-temporal change detection involves the analysis of raster images acquired at different time periods
 ## to identify and quantify changes in land use, vegetation cover, or other environmental phenomena over time.
 
 # Specify a color scheme and plot the resulting image 
-cl2 <- colorRampPalette(c("brown", "white", "orange")) (100)
+cl2 <- colorRampPalette(c("brown", "white", "orange"))(100)
 plot(amazondif, col=cl2)
 
 dev.off()
 
 ## Calculate vegetation indices from remote sensing (RS) data
 
-# Recall the Rstoolbox package 
-library(RStoolbox)
-
 # Use the unsuperClass() function for the unsupervised classification of 2000 data
 # and specify the number of classes (class 1 = forest, class 2 = human impact)
-d1c <- unsuperClass(amazon2000, nClasses=2)
+d1c <- unsuperClass(amazon2001, nClasses=2)
 plot(d1c$map)
 
 # To analyze the distribution of values within the raster image use the freq() function
-freq(d1c$map)
+val2001 <- freq(d1c$map)
 
 #  layer value   count
 # 1     1     1  468953
@@ -139,25 +140,31 @@ freq(d1c$map)
 ## Class 1: human impact = 468953
 ## Class 2: forest = 1771047
 
-# Forest component of 2000
-f2000 <- 1771047 / (1771047 + 468953) # or f2000 <- d1c$map[[2]] / (d1c$map[[2]] + d1c$map[[1]]) 
-f2000
-p2000 <- f200 * 100
+# Forest component of 2001
+f2001 <- val2001[2,3] / (val2001[2,3] + val2001[1,3])
+f2001
 
-## f2000 = 0,790646 = 79,06%
+# Compute the forest percentage of 2001
+percent_f2001 <- f2001 * 100
+percent_f2001
 
-# Human component of 2000
-h2000 <- 468953 / (1771047 + 468953) # or f2000 <- d1c$map[[1]] / (d1c$map[[2]] + d1c$map[[1]]) 
-h2000
-p2012 <- h2000 * 100
+## f2001 = 0,790646 = 79,06%
 
-## h2000 = 0,209354 = 20,94%
+# Human component of 2001
+h2001 <- val2001[1,3] / (val2001[2,3] + val2001[1,3])
+h2001
+
+# Human percentage of 2001
+percent_h2001 <- h2001 * 100
+percent_h2001
+
+## h2001 = 0,209354 = 20,94%
 
 # Now compute the same classification for 2012 data
 d2c <- unsuperClass(amazon2012, nClasses=2)
 plot(d2c$map)
 
-freq(d2c$map)
+val2012 <- freq(d2c$map)
 
 #  layer value   count
 # 1     1     1  636865
@@ -167,80 +174,47 @@ freq(d2c$map)
 ## Class 2: forest = 1603135
 
 # Forest component of 2012
-f2012 <- 636865 / (1603135 + 636865) # or f2012 <- d2c$map[[1]] / (d2c$map[[2]] + d2c$map[[1]]) 
+f2012 <- val2012[2,3] / (val2012[2,3] + val2012[1,3])
 f2012
+
+# Forest percentage of 2012
+percent_f2012 <- f2012 * 100
+percent_f2012
 
 ## f2012 =  0,2843147 = 28,43%
 
 # Human component of 2012
-h2012 <- 1603135 / (1603135 + 636865)
+h2012 <- val2012[1,3] / (val2012[2,3] + val2012[1,3])
 h2012
+
+# Human percentage of 2012
+percent_h2012 <- h2012 * 100
+percent_h2012
 
 ## h2012 = 0,7156853 = 71,57%
 
-# Create the final table to assess the changes in land use and cover with the data.frame() function
+# Create the final table to assess land use and cover changes with the data.frame() function
 landcover <- c("Forest", "Humans")
-percent_2000 <- c(79.06, 20.94)
-percent_2012 <- c(28.43, 71.57)
-
-percentage <- data.frame(landcover, percent_2000, percent_2012)
+percent_2001 <- c(percent_f2001, percent_h2001)
+percent_2012 <- c(percent_f2012, percent_h2012)
+percentage <- data.frame(landcover, percent_2001, percent_2012)
 percentage
 
 #     landcover percent_2000 percent_2012
 # 1    Forest        79.06        28.43
 # 2    Humans        20.94        71.57
 
-# Recall the ggplot2 package 
-library(ggplot2)
-
-# and plot data within a histogram 
-ggplot(percentage, aes(x=landcover, y=percent_2000, color=landcover)) + geom_bar(stat="identity", fill="white")
+# Plot data within a histogram with the ggplot() function of the ggplot package
+ggplot(percentage, aes(x=landcover, y=percent_2001, color=landcover)) + geom_bar(stat="identity", fill="white")
 ggplot(percentage, aes(x=landcover, y=percent_2012, color=landcover)) + geom_bar(stat="identity", fill="white")
 
-# Recall the package called patchwork
-library(patchwork)
+# Assign the two plots to the objects p1 and p2 
+p1 <- ggplot(percentage, aes(x=landcover, y=percent_2001, color=landcover)) + geom_bar(stat="identity", fill="white") + coord_cartesian(ylim=c(0, 100))
+p2 <- ggplot(percentage, aes(x=landcover, y=percent_2012, color=landcover)) + geom_bar(stat="identity", fill="white") + coord_cartesian(ylim=c(0, 100))
 
-# Assign the two plots to an object 
-p1 <- ggplot(percentage, aes(x=landcover, y=percent_2000, color=landcover)) + geom_bar(stat="identity", fill="white")
-p2 <- ggplot(percentage, aes(x=landcover, y=percent_2012, color=landcover)) + geom_bar(stat="identity", fill="white")
+## I use the coord_cartesian() function to set the same scale
 
 # Plot the two histograms and compare them 
 p1 + p2
 
 dev.off()
-
-####################################################################################################################
-
-plotRGB(amazon2000, r=1, g=2, b=3)
-nir <- amazon2000[[1]]
-plot(nir)
-
-sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
-plot(sd3)
-
-viridisc <- colorRampPalette(viridis(7))(255)
-plot(sd3, col=viridisc)
-
-sd7 <- focal(nir, matrix(1/49, 7, 7), fun=sd)
-plot(sd7, col=viridisc)
-
-par(mfrow=c(1,2))
-plot(sd3, col=viridisc)
-plot(sd7, col=viridisc)
-
-plotRGB(amazon2012, r=1, g=2, b=3)
-nir <- amazon2012[[1]]
-plot(nir)
-
-sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
-plot(sd3)
-
-viridisc <- colorRampPalette(viridis(7))(255)
-plot(sd3, col=viridisc)
-
-sd7 <- focal(nir, matrix(1/49, 7, 7), fun=sd)
-plot(sd7, col=viridisc)
-
-par(mfrow=c(1,2))
-plot(sd3, col=viridisc)
-plot(sd7, col=viridisc)
